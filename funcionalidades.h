ee9dcfc07789d5a3cf65013f1ed99a0b2347c303
SziuL <luizsabadinis@gmail.com>
2023-11-14 16:40:49 -0300

    Updates: 	modified:   funcionalidades.h Untracked files: 	.gitignore

--- funcionalidades.h
+++ funcionalidades.h
@@ -18,20 +18,17 @@ void excluirFilial(Filial *filiais, int idOrigem, int idDestino);
 void operacoesFilial(Filial *filiais, Livro *livros);
 
 // complementares
-// void transferirBaseDados(Filial *L, int idAntiga, int idNova);
 int encriptar(char isbn[5]);
 Filial *buscaFilial(Filial *filiais, int id, Filial **pred);
 Livro *menor(Livro *livro);
 Livro *maior(Livro *livro);
-Livro *sucessor(Livro *livro);
-Livro *predecessor(Livro *livro);
 Livro *coletaDadosNovoLivro();
 void imprimeIsbn(char *isbn, int n);
 void selecionaFilial(Filial *filiais, int id);
 void inserirFilial(Filial *filiais, Filial *novaFilial);
 void percorreLivros(Livro *livrosOrigem, Livro *baseDadosDestino);
 
-    // programa principal
-    void programa(Filial *filiais);
+// programa principal
+void programa(Filial *filiais);
 
 #endif
